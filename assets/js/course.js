d72414dad7e104c35227aaaba34328ab4d560a19
YourNerdiness <darcyjsutton7@gmail.com>
2024-01-25 18:23:14 +1100

    implemented match-tight exercise

--- assets/js/course.js
+++ assets/js/course.js
@@ -1,5 +1,26 @@
 let time;
 
+const utils = {
+
+    shuffleArray : (arr=[]) => {
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+
+            const j = Math.floor(Math.random()*(i + 1));
+
+            const temp = arr[j];
+
+            arr[j] = arr[i];
+            arr[i] = temp;
+
+        }
+
+        return arr;
+
+    }
+
+}
+
 const sendSessionTimetoServer = (sessionTime=(Date.now()-Number(localStorage.getItem("sessionStartTime")))) => {
 
     fetch('/logSessionTime', {
@@ -109,6 +130,170 @@ window.onload = async () => {
         case "e":
 
             const exerciseData = await (await fetch("https://coursecontent.aristotle.academy" + contentIDParts[0])).json();
+
+            $("#continue").prop("disabled", true);
+
+            switch (exerciseData.type) {
+
+                case "match-tight":
+
+                    localStorage.removeItem("match_tight_last_column_one_clicked");
+                    localStorage.removeItem("match_tight_last_column_two_clicked");
+                    localStorage.removeItem("match_tight_num_matched");
+
+                    const div = document.getElementById("exercise");
+
+                    const pairs = exerciseData.data.pairs;
+                    const description = exerciseData.data.description;
+
+                    const p = document.createElement("p");
+
+                    p.textContent = description;
+                    p.classList = "center"
+
+                    div.appendChild(p);
+
+                    const columnOne = pairs.map(x => x[0]);
+                    const columnTwo = pairs.map(x => x[1]);
+
+                    utils.shuffleArray(columnOne);
+                    utils.shuffleArray(columnTwo);
+
+                    const table = document.createElement("table");
+
+                    div.appendChild(table);
+
+                    for (let i = 0; pairs.length; i++) {
+
+                        const tableRow = document.createElement("tr");
+
+                        const columnOneElem = document.createElement("td");
+                        const columnTwoElem = document.createElement("td");
+
+                        const columnOneBtn = document.createElement("button");
+                        const columnTwoBtn = document.createElement("button");
+
+                        columnOneBtn.textContent = columnOne[i];
+                        columnTwoBtn.textContent = columnTwo[i];
+
+                        columnOneBtn.addEventListener("click", () => { 
+
+                            if (columnTwo[i] == localStorage.getItem("match_tight_last_column_two_clicked")) { 
+
+                                columnOneBtn.disabled = true; 
+                                columnTwoBtn.disabled = true;
+
+                                columnOneBtn.style.backgroundColor = "";
+                                columnTwoBtn.style.backgroundColor = "";
+
+                                localStorage.removeItem("match_tight_last_column_one_clicked");
+                                localStorage.removeItem("match_tight_last_column_two_clicked");
+
+                                localStorage.setItem("match_tight_num_matched", (localStorage.getItem("match_tight_num_matched") || 0) + 1);
+
+                                if (localStorage.getItem("match_tight_num_matched") >= pairs.length) {
+
+                                    $("#continue").prop("disabled", false);
+
+                                }
+                            
+                            }
+
+                            else {
+
+                                if (localStorage.getItem("match_tight_last_column_two_clicked")) {
+
+                                    columnOneBtn.disabled = false; 
+                                    columnTwoBtn.disabled = false;
+
+                                    columnOneBtn.style.backgroundColor = "";
+                                    columnTwoBtn.style.backgroundColor = "";
+
+                                    localStorage.removeItem("match_tight_last_column_one_clicked");
+                                    localStorage.removeItem("match_tight_last_column_two_clicked");
+
+                                }
+
+                                else {
+
+                                    columnOneBtn.disabled = true;
+                                    columnOneBtn.style.backgroundColor = "lightgray";
+
+                                    localStorage.setItem("match_tight_last_column_one_clicked", columnOne[i]);
+
+                                }
+
+                            }
+
+                        });
+
+                        columnTwoBtn.addEventListener("click", () => { 
+
+                            if (columnTwo[i] == localStorage.getItem("match_tight_last_column_one_clicked")) { 
+
+                                columnOneBtn.disabled = true; 
+                                columnTwoBtn.disabled = true;
+
+                                columnOneBtn.style.backgroundColor = "";
+                                columnTwoBtn.style.backgroundColor = "";
+
+                                localStorage.removeItem("match_tight_last_column_one_clicked");
+                                localStorage.removeItem("match_tight_last_column_two_clicked");
+
+                                localStorage.setItem("match_tight_num_matched", (localStorage.getItem("match_tight_num_matched") || 0) + 1);
+
+                                if (localStorage.getItem("match_tight_num_matched") >= pairs.length) {
+
+                                    $("#continue").prop("disabled", false);
+
+                                }
+                            
+                            }
+
+                            else {
+
+                                if (localStorage.getItem("match_tight_last_column_one_clicked")) {
+
+                                    columnOneBtn.disabled = false; 
+                                    columnTwoBtn.disabled = false;
+
+                                    columnOneBtn.style.backgroundColor = "";
+                                    columnTwoBtn.style.backgroundColor = "";
+
+                                    localStorage.removeItem("match_tight_last_column_one_clicked");
+                                    localStorage.removeItem("match_tight_last_column_two_clicked");
+
+                                }
+
+                                else {
+
+                                    columnTwoBtn.disabled = true;
+                                    columnTwoBtn.style.backgroundColor = "lightgray";
+
+                                    localStorage.setItem("match_tight_last_column_two_clicked", columnOne[i]);
+
+                                }
+
+                            }
+
+                        });
+
+                        tableRow.appendChild(columnOneElem);
+                        tableRow.appendChild(columnTwoElem);
+
+                        table.appendChild(tableRow);
+
+                    }
+
+                    break
+                    
+                default:
+
+                    $("#error").text("Invalid exercise type.");
+
+                    break;
+
+            }
 
             $("#video").hide()
             $("#paragraph").hide()
